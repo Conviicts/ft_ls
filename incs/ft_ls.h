6d2067d9e04b37d8d8d6d82e4b15cebf4aeb27db
jode-vri <jode-vri@student.42.fr>
2023-11-22 00:18:49 +0100

    test args with va_arg

---
+++ incs/ft_ls.h
@@ -0,0 +1,10 @@
+#ifndef FT_LS_H
+#define FT_LS_H
+
+typedef struct	s_ls
+{
+	char	*test;
+}				t_ls;
+
+
+#endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,13 +1,20 @@
+#include "ft_ls.h"
 #include "libft.h"
+#include <stdarg.h>
 
-void func() {
-    printf("Function for option1\n");
+void func(va_list args) {
+    t_ls *st = va_arg(args, t_ls *);
+
+    printf("Function called with args: %s\n", st->test);
 }
 
 int main(int ac, char **av) {
 	(void)ac;
 	(void)av;
-	t_args mappings[] = {
+
+	t_ls		ls = { .test = "kjgkj" };
+
+	t_args		mappings[] = {
         { "-l", func },
         { "-R", func },
         { "-a", func },
@@ -20,5 +27,12 @@ int main(int ac, char **av) {
         .len = sizeof(mappings) / sizeof(mappings[0])
     };
 
-	ft_parseargs(args, av[1]);
+	for (int i = 1; i < ac; i++) {
+        char *option = av[i];
+        if (ft_parseargs(args, option, &ls)) {
+            // Option found and processed
+        } else {
+            // Option not found
+        }
+    }
 }
